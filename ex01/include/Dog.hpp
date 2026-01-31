413845e659a4246c7fdedbef100496f1f94a02d7
Yohen Sheraun <yosherau@u91z01s01.42kl.edu.my>
2026-01-31 18:59:30 +0800

    refactor: placed brain in private, and removed wrong lines

--- ex01/include/Dog.hpp
+++ ex01/include/Dog.hpp
@@ -6,7 +6,7 @@
 /*   By: yosherau <yosherau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 13:08:36 by yosherau          #+#    #+#             */
-/*   Updated: 2026/01/22 16:55:23 by yosherau         ###   ########.fr       */
+/*   Updated: 2026/01/31 18:52:31 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@
 
 class Dog: public Animal
 {
+	Brain	*itsBigBrainTime;
 	
 	public:
-		Brain	*itsBigBrainTime;
 		Dog();
 		~Dog();
 		Dog(const Dog &original);
--- ex01/srcs/Brain.cpp
+++ ex01/srcs/Brain.cpp
@@ -6,7 +6,7 @@
 /*   By: yosherau <yosherau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 20:43:58 by yosherau          #+#    #+#             */
-/*   Updated: 2026/01/31 18:45:48 by yosherau         ###   ########.fr       */
+/*   Updated: 2026/01/31 18:55:17 by yosherau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,8 +25,6 @@ Brain::~Brain()
 Brain::Brain(const Brain &other)
 {
 	std::cout << "Brain's copy constructor was called" << std::endl;
-	if (this == &other)
-		return ;
 	for (int index = 0; index < IDEAS_AMNT; index++)
 		this->ideas[index] = other.ideas[index];
 }
@@ -46,7 +44,7 @@ void Brain::set_idea(int index, std::string idea)
 {
 	if (idea.empty())
 		std::cout << "Idea's can't be empty" << std::endl;
-	else if (index < 0 || index >= IDEAS_AMNT)
+	else if (index < 0 || index >= IDEAS_AMNT - 1)
 		std::cout << "Index specified is of an invalid range" << std::endl;
 	else
 		this->ideas[index] = idea; 
